e88200bc11e92909713a9494702905e74eb41899
Andrew Keith <38077188+crankeith@users.noreply.github.com>
2018-11-18 20:38:38 -0500

    First round of functionality
    Includes additional quotes, tagging and some HTML updates.

--- js/script.js
+++ js/script.js
@@ -18,67 +18,77 @@ FSJS project 1 - A Random Quote Generator
 
 let quotes = [
     {
-        quote: 'Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.',
-        source: 'Mark Twain',
-        year: 1860,
-        citation: ''
+        quote: "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.",
+        source: "Mark Twain",
+        tags: ["Writer", "Inspiration"]
     },
     {
-        quote: 'Get busy living or get busy dying.',
-        source: 'Stephen King',
-        year: 1960,
-        citation: ''
+        quote: "Get busy living or get busy dying.",
+        source: "Stephen King",
+        tags: ["Writer", "Fun"]
     },
     {
-        quote: 'I/’m a success today because I had a friend who believed in me and I didn/’t have the heart to let him down.',
-        source: 'Abraham Lincoln',
-        year: 1999,
-        citation: ''
+        quote: "I’m a success today because I had a friend who believed in me and I didn’t have the heart to let him down.",
+        source: "Abraham Lincoln",
+        tags: ["Politician", "Inspiration"]
     },
     {
-        quote: 'It is our choices, that show what we truly are, far more than our abilities.',
-        source: 'J.K Rowling',
-        year: 1999,
-        citation: ''
+        quote: "It is our choices, that show what we truly are, far more than our abilities.",
+        source: "J.K Rowling",
+        tags: ["Writer", "Philosophical"]
+    },
+    {
+        quote: "Do or do not. There is no try.",
+        source: "Yoda",
+        citation: "Star Wars: Episode V - The Empire Strikes Back",
+        year: 1980,
+        tags: ["Fictional", "Inspiration"]
+    },
+    {
+        quote: "Welcome to the party, Pal!",
+        source: "John Mclean",
+        citation: "Die Hard",
+        year: 1988,
+        tags: ["Fictional", "Fun"]
     }
 ]
 
-/***
-  Create the `getRandomQuote` function to:
-   - generate a random number 
-   - use the random number to `return` a random quote object from the 
-     `quotes` array.
-***/
-
+// Function that returns a random quote object from the 'quotes' array
 function getRandomQuote(arr) {
-    let randomNum = Math.floor(Math.random() * arr.length)
-
-    return randomNum
+    let randomNum = Math.floor(Math.random() * arr.length);
+    return arr[randomNum];
 }
 
-console.log(getRandomQuote(quotes));
+// Function to print a random quote to the 'quote-box' div
+function printQuote(){
+    let quote = getRandomQuote(quotes);
+    let html = '<p class="quote">';
+    html += quote.quote + '</p>';
+    html += '<p class="source">' + quote.source;
 
-/***
-  Create the `printQuote` function to: 
-   - call the `getRandomQuote` function and assign it to a variable.
-   - use the properties of the quote object stored in the variable to 
-     create your HTML string.
-   - use conditionals to make sure the optional properties exist before 
-     they are added to the HTML string.
-   - set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
+    //Check if quote has a citation and add if available
+    if (quote.citation) {
+        html += '<span class="citation">' + quote.citation + '</span>';
+    }
+    //Check if quote has a year and add if available
+    if (quote.year) {
+        html += '<span class="year">' + quote.year + '</span>';
+    }
 
+    if (quote.tags) {
+        html += '<span class="tags">Categories: '
+        for(let i = 0; i < quote.tags.length; i++){
+            html += '<span class="tag">' + quote.tags[i] + '</span>'
+        }
+        html += '</span>'
+    }
 
+    html += '</p>';
 
+    document.getElementById('quote-box').innerHTML = html;
+}
 
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
+// Add click listener to button
 
-//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
